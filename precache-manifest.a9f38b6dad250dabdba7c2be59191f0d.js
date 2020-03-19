b556aa62fbd0674732f60e18f2105e43ce11b604
Alexey <alex.hanko@mail.ru>
2020-03-19 19:38:40 +0300

    Updates

---
+++ precache-manifest.a9f38b6dad250dabdba7c2be59191f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13543eedfceadf1f10f75e329e8589f9",
+    "url": "/simple-react-card-app/index.html"
+  },
+  {
+    "revision": "fcf64a5f9d12332d5e78",
+    "url": "/simple-react-card-app/static/css/main.9fee815e.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/simple-react-card-app/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/simple-react-card-app/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf64a5f9d12332d5e78",
+    "url": "/simple-react-card-app/static/js/main.9956a3a9.chunk.js"
+  },
+  {
+    "revision": "2aeb1ee7bd4a9f7fd5fa",
+    "url": "/simple-react-card-app/static/js/runtime-main.85c86cc6.js"
+  }
+]);
